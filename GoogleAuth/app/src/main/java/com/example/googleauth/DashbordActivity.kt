a34b4d5c8bdfa34f3ec3036fbac457e45c8db980
Cipppp <pirvuciprian37@gmail.com>
2020-07-26 23:58:18 +0300

    Sign in with firebase

---
+++ GoogleAuth/app/src/main/java/com/example/googleauth/DashbordActivity.kt
@@ -0,0 +1,11 @@
+package com.example.googleauth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DashbordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashbord)
+    }
+}
---
+++ GoogleAuth/app/src/main/java/com/example/googleauth/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.googleauth
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mAuth = FirebaseAuth.getInstance()
+        val user = mAuth.currentUser
+//        If user is not authenticated, send him to SignInActivity to authenticate first. Else send him to DashbordActivity
+        Handler().postDelayed({
+            if (user != null)  {
+                val dashbordIntent = Intent(this, DashbordActivity::class.java)
+                startActivity(dashbordIntent)
+            } else {
+                val signInIntent = Intent(this, SignInActivity::class.java)
+                startActivity(signInIntent)
+            }
+        }, 2000)
+    }
+}
