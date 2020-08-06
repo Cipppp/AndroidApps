49371b0d2320c1ef788836065a5563ac292ce0ef
Cipppp <pirvuciprian37@gmail.com>
2020-08-06 11:24:03 +0300

    Added

--- Aboutme/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ Aboutme/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.example.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    val myName: MyName = MyName("Cipp")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myName
+//        findViewById<Button>(R.id.done_button).setOnClickListener {
+//            addNickname(it)
+//        }
+
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+
+        binding.apply {
+//            binding.nicknameText.text = binding.nicknameEdit.text
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            binding.nicknameEdit.visibility = View.GONE
+            binding.doneButton.visibility = View.GONE
+            binding.nicknameText.visibility = View.VISIBLE
+        }
+
+        // Hide the keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
