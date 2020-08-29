eb345ec8f6b879535821e23f4e4a34155cb49f18
Cipppp <pirvuciprian37@gmail.com>
2020-08-29 23:04:30 +0300

    Updated

--- HappyBirthdayApp/app/src/main/java/com/example/happybirthdayapp/MainActivity.kt
+++ HappyBirthdayApp/app/src/main/java/com/example/happybirthdayapp/MainActivity.kt
@@ -19,31 +19,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         cake_button.setOnClickListener {
-            wishTextView.text = "Happy birthday Cipp"
+            wishTextView.visibility = View.VISIBLE
+            animation.visibility = View.VISIBLE
         }
 
-
-        // Arc button
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-//            val path = Path().apply {
-//                arcTo(0f, 0f, 600f, 1200f, 90f, -180f, true)
-//            }
-//            val animator = ObjectAnimator.ofFloat(cake_button, View.X, View.Y, path).apply {
-//                duration = 2000
-//                start()
-//            }
-//        } else {
-//            // Create animator without using curved path
-//        }
-
-
-
-        findViewById<View>(R.id.imageView).also { img ->
-            SpringAnimation(img, DynamicAnimation.TRANSLATION_Y).apply {
-                //Setting the damping ratio to create a low bouncing effect.
-                spring.dampingRatio = SpringForce.DAMPING_RATIO_LOW_BOUNCY
-            }
-        }
 
 
 
